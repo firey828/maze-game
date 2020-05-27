c91807d8c6947c6647a2c6b0b82aed2b74b0cacd
firey828 <47870754+firey828@users.noreply.github.com>
2020-05-27 14:54:23 -0400

    finished comments for everything in the stages package

--- epic-maze-game/src/com/company/stages/Stage.java
+++ epic-maze-game/src/com/company/stages/Stage.java
@@ -23,7 +23,24 @@ public interface Stage {
      * Returns true if a call of amIAt() from the Stage's Player returns true; false otherwise
      */
     public boolean playerIsAtEnd();
+
+    /*
+     * Returns the status of the in-bounds square located at _maze[r][c]. isWall(Position) implements this same
+     * functionality and more, so it would be more efficient to simply use that. However, nobody said this was
+     * an efficient program, and I never said I was an efficient programmer, so here we all are.
+     */
     public boolean isWall(int r, int c);
+
+    /*
+     * Returns the status of the presumably in-bounds Position p. (true if it is a wall, false otherwise.)
+     * If p is NOT in bounds, we will just say it's a wall (after all, for all practical purposes except
+     * those of the game's graphics, it is.)
+     */
     public boolean isWall(Position p);
+
+    /*
+     * Exactly what's written on the tin. Returns true if the Position p is wtihin the bounds of our current
+     * _maze, false otherwise.
+     */
     public boolean isInBounds(Position p);
 }
